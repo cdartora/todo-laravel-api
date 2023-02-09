2a5cdda8f3f291c503bf3502309e00ec99674c10
Carlos Dartora <carlosdalsoler@gmail.com>
2023-02-09 19:01:55 -0300

    feat: create the route for get all tasks

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return response()->json(['message' => 'hello, world. im a simple todo rest api that can manage your tasks.']);
 });
+
+Route::get('/tasks', [TodoController::class, 'index']);
