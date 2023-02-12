dd9265b0eb858b5b84726c05ad83441c3801204f
Carlos Dartora <carlosdalsoler@gmail.com>
2023-02-12 16:53:12 -0300

    feat: add guarded anf fillable properties to the Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    protected $guarded = [
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+
     use HasFactory;
 }
