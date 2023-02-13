27ebdbb1868bfeb9ffe5de7ba2d3aed026aa521d
Carlos Dartora <carlosdalsoler@gmail.com>
2023-02-13 14:43:52 -0300

    feat: create the update and delete route and add validation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Task; // import Task model for db querys
+use App\Models\Task;
+use Validator;
+use League\Config\Exception\ValidationException; // import Task model for db querys
 
 class TaskController extends Controller
 {
@@ -26,12 +28,27 @@ class TaskController extends Controller
     public function store(Request $request) // uses the Request class as an argument
 
     {
-        $task = Task::create([
+        $data = $request->all();
+
+        // this is the default validation method in laravel
+        $validator = Validator::make(
+            $data,
+            [
+                'title' => 'required|min:5|max:255',
+                'description' => 'nullable|max:255',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        Task::create([
             'title' => $request->input('title'),
             'description' => $request->input('description'),
         ]);
 
-        return response()->json(['task' => $task], 201);
+        return response()->json(['message' => 'Todo created successfully.'], 201);
     }
 
     /**
@@ -42,19 +59,8 @@ class TaskController extends Controller
      */
     public function show($id)
     {
-        $task = Task::find($id);
+        $task = Task::findOrFail($id);
         return response()->json($task, 200);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
@@ -66,7 +72,29 @@ class TaskController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // search for the task, if dont find it will throw exception
+        $task = Task::findOrFail($id);
+
+        $data = $request->all();
+
+        // this is the default validation method in laravel
+        $validator = Validator::make(
+            $data,
+            [
+                'title' => 'required|min:5|max:255',
+                'description' => 'nullable|max:255',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // here array filter is used to remove empty field
+        // in case description came empty
+        $task->update(array_filter($data));
+
+        return response()->json(['message' => 'Todo was updated successfully.'], 200);
     }
 
     /**
@@ -77,6 +105,8 @@ class TaskController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return response()->json(['message' => 'Todo was deleted successfully.']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,7 @@ Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
 
 Route::get('/tasks/{id}', [TaskController::class, 'show']);
+
+Route::put('/tasks/{id}', [TaskController::class, 'update']);
+
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
